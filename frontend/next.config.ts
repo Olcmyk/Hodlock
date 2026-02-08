4949d93676f5a7d1f1c21f92a4c732cbe35bf95d
Olcmyk <60802501+Olcmyk@users.noreply.github.com>
2026-02-08 18:30:12 +0800

    for 4everland deploy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // 关键修改：开启静态导出，生成 4EVERLAND 需要的 out 文件夹
+  trailingSlash: true,
   images: {
     unoptimized: true, // 你已经写了这一行，非常好，这是 IPFS 部署必须的
   },
